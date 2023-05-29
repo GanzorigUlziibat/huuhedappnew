f601045a055c0bd96fc6b2cca1d1c2efaa7e7ba4
chillwithdrjj <sw21d037@mandakh.edu.mn>
2023-05-29 15:48:38 +0800

    Update Delgerengui.js

--- babyexpo/src/screens/Delgerengui.js
+++ babyexpo/src/screens/Delgerengui.js
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Platform,
+} from "react-native";
+import React from "react";
 
 export default function Delgerengui() {
   return (
-    <View>
-      <Text>Delgerengui</Text>
-    </View>
-  )
+    <SafeAreaView>
+      <ScrollView style={style.container}>
+        <View>
+          <Text>Delgerengui</Text>
+          <Image
+            style={styles.family}
+            source={require("../images/fox.png")}
+          ></Image>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  family: {
+    width: 80,
+    height: 70,
+  },
+});
