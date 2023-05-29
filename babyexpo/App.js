fb9fedecd52d2fdf6de5fa2501b72fc034c9267f
IaBaseDi247 <112841024+IaBaseDi24@users.noreply.github.com>
2023-05-29 15:48:32 +0800

    update

--- babyexpo/App.js
+++ babyexpo/App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import Tabs from "./src/screens/Tabs";
 import Delgerengui from "./src/screens/Delgerengui";
+import Delgerengui2 from "./src/screens/Delgerengui2";
 const HomeStack = createNativeStackNavigator();
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       >
         <HomeStack.Screen name="Tabs" component={Tabs} />
         <HomeStack.Screen name="Delgerengui" component={Delgerengui} />
+        <HomeStack.Screen name="Delgerengui2" component={Delgerengui2} />
       </HomeStack.Navigator>
     </NavigationContainer>
   );
