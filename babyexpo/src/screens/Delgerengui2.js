516a1edab23f56e85b76b5bdf119eda3abb67beb
IaBaseDi247 <112841024+IaBaseDi24@users.noreply.github.com>
2023-05-29 14:47:01 +0800

    update

---
+++ babyexpo/src/screens/Delgerengui2.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Delgerengui2() {
+  return (
+    <View>
+      <Text>Delgerengui2</Text>
+    </View>
+  )
+}
